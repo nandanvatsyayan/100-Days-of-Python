5f9da61a47cdf0bdd62a577087965ac4228a9b9d
Nandan Vatsyayan <42815434+nandanvatsyayan@users.noreply.github.com>
2024-10-18 15:48:00 +0530

    Add files via upload
    Print Squre Of a List

---
+++ Day 2/Print Squre Of  A List.py
@@ -0,0 +1,9 @@
+numbers = [4, 2, 6, 7, 3, 5, 8, 10, 6, 1, 9, 2]  
+square = 0 
+squares = []  
+for value in numbers:  
+    square = value ** 2  
+    squares.append(square)  
+print("The list of squares is", squares)  
+
+
