f9a51e8cac2e3a85ee3effe3332dbdc1d659dfce
Nandan Vatsyayan <42815434+nandanvatsyayan@users.noreply.github.com>
2024-10-18 15:39:05 +0530

    Add files via upload
    Function to check if a year is a Leap Year or Not

---
+++ Day 2/leap_year.py
@@ -0,0 +1,16 @@
+def leap_year(year):
+    if (year % 4 == 0):
+        if (year % 100 == 0):
+            if (year % 400 == 0):
+                return True
+            else:
+                return False
+        else:
+            return True
+    else:
+        return False
+year = int(input("Enter a year"))
+if leap_year(year):
+    print("It is a leap year")
+else:
+    print("It is not a leap year")
